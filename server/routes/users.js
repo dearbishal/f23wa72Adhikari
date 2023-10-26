ffb77571b16484e4bb2477697437de3747a3b6db
Bishal Adhikari <99350850+dearbishal@users.noreply.github.com>
2023-10-25 21:04:42 -0500

    Edited users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+let sum = 0;
+let more = 0;
+
+
+router.get('/', (req, res) => {
+  more += 1;
+
+  sum += more;
+
+
+  res.send(`Sum is: ${sum}`);
 });
 
 module.exports = router;
